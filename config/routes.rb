e4eaa18ece830d17c768132bd5b3147a8afcba19
Yaraslau Maksimchyk <90287515+iaruso@users.noreply.github.com>
2022-03-15 17:17:14 +0000

    6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get 'hello', to: 'pages#contact'
-  get 'pages/about'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 end
